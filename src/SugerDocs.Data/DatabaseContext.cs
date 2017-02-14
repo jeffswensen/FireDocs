5cd445a4a6d3403146bd7d21f22df509b6757908
Jeffery Swensen <jswensen@21cf.com>
2017-02-13 21:07:50 -0500

    fluent entity config:  keys and autogen

--- src/SugerDocs.Data/DatabaseContext.cs
+++ src/SugerDocs.Data/DatabaseContext.cs
@@ -11,23 +11,23 @@ namespace SugerDocs.Data {
      */
     public class SugerContext : DbContext, ISugerContext {
 
-        public DbSet<Department> Departments { get; }
+        public DbSet<Department> Departments { get; set; }
 
-        public DbSet<DepartmentRole> DepartmentRoles { get; }
+        public DbSet<DepartmentRole> DepartmentRoles { get; set; }
 
-        public DbSet<Document> Documents { get; }
+        public DbSet<Document> Documents { get; set; }
 
-        public DbSet<DocumentRole> DocumentRoles { get; }
+        public DbSet<DocumentRole> DocumentRoles { get; set; }
 
-        public DbSet<Organization> Organizations { get; }
+        public DbSet<Organization> Organizations { get; set; }
 
-        public DbSet<OrganizationRole> OrganizationRoles { get; }
+        public DbSet<OrganizationRole> OrganizationRoles { get; set; }
 
-        public DbSet<SiteRole> SiteRoles { get; }
+        public DbSet<SiteRole> SiteRoles { get; set; }
 
-        public DbSet<Role> Roles { get; }
+        public DbSet<Role> Roles { get; set; }
 
-        public DbSet<User> Users { get; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.Entity<DepartmentRole>()
@@ -38,6 +38,61 @@ namespace SugerDocs.Data {
                 .HasAlternateKey(or => new { or.RoleId, or.UserId, or.OrganizationId });
             modelBuilder.Entity<SiteRole>()
                 .HasAlternateKey(sr => new { sr.RoleId, sr.UserId });
+
+            modelBuilder.Entity<Department>()
+                .HasKey(d => d.DepartmentId);
+            modelBuilder.Entity<Department>()
+                .Property(d => d.DepartmentId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<DepartmentRole>()
+                .HasKey(dr => dr.DepartmentRoleId);
+            modelBuilder.Entity<DepartmentRole>()
+                .Property(dr => dr.DepartmentRoleId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Document>()
+                .HasKey(e => e.DocumentId);
+            modelBuilder.Entity<Document>()
+                .Property(e => e.DocumentId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<DocumentRole>()
+                .HasKey(e => e.DocumentRoleId);
+            modelBuilder.Entity<DocumentRole>()
+                .Property(e => e.DocumentRoleId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Organization>()
+                .HasKey(e => e.OrganizationId);
+            modelBuilder.Entity<Organization>()
+                .Property(e => e.OrganizationId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<OrganizationRole>()
+                .HasKey(e => e.OrganizationRoleId);
+            modelBuilder.Entity<OrganizationRole>()
+                .Property(e => e.OrganizationRoleId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<Role>()
+                .HasKey(e => e.RoleId);
+            modelBuilder.Entity<Role>()
+                .Property(e => e.RoleId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<SiteRole>()
+                .HasKey(e => e.SiteRoleId);
+            modelBuilder.Entity<SiteRole>()
+                .Property(e => e.SiteRoleId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<User>()
+                .HasKey(e => e.UserId);
+            modelBuilder.Entity<User>()
+                .Property(e => e.UserId)
+                .ValueGeneratedOnAdd();
+
         }
     }
 
